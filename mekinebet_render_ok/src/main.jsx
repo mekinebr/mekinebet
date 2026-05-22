ce8a648eabc7d5bc24c9e283844c8edb70d57607
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-05-22 15:10:56 -0400

    Delete mekinebet_render_ok directory

--- mekinebet_render_ok/src/main.jsx
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("root").innerHTML = `
-  <div style="font-family:Arial;padding:40px;text-align:center">
-    <h1>Mekinebet Online</h1>
-    <p>Projeto funcionando no Render.</p>
-  </div>
-`;
