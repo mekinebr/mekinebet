a5fe3e5b4138340d43a01ee6987d9f75de4ba6f7
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-05-27 11:40:42 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,37 @@
 self.addEventListener("install", () => {
-  console.log("Service Worker instalado");
+  console.log("SW instalado");
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("activate", () => {
+  console.log("SW ativo");
+});
+
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+
+  self.registration.showNotification(
+    data.title,
+    {
+      body: data.body,
+      icon: "/logo192.png",
+      badge: "/logo192.png",
+      vibrate: [200, 100, 200],
+      data: {
+        url: data.url
+      }
+    }
+  );
+});
+
+self.addEventListener(
+  "notificationclick",
+  (event) => {
+    event.notification.close();
+
+    event.waitUntil(
+      clients.openWindow(
+        event.notification.data.url
+      )
+    );
+  }
+);
