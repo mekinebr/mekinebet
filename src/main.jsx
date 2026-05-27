e7b52aa4b31c3a44ee6b803384ee2da9e3ac19c0
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-05-26 23:03:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => {
+        console.log(
+          "Service Worker registrado"
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
