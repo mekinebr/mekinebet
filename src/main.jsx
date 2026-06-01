ea6eb86a4e7d16cb3cd2e12507baba71df3e991b
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-06-01 15:38:08 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Renderização principal
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
 );
 
-// Limpa Service Worker antigo e caches antigos
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
